664cedaf9ce4d395af89f98992d358bd03c06195
Fran Moreno <fran.moreno.se@gmail.com>
2024-08-20 13:07:38 +0200

    Added Resume section, made backend changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,21 @@ function loadPartial(id, url, callback = null) {
       .catch(error => console.error('Error loading partial:', error));
   }
 
+function openTab(event, tab_name, section) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName(section + "_tabcontent");
+    tablinks = document.getElementsByClassName(section + "_tablink");
+    for (i = 0; i < tabcontent.length; i++) {
+      /* 1. Reset visible tab */
+      tabcontent[i].style.display = "none";
+      /* 2. Reset buttons state*/
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(tab_name).style.display = "block";
+    event.currentTarget.className += " active";
+  }
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
@@ -20,11 +35,38 @@ function projectsScript() {
     .then((json) => json.forEach((element) => console.log(element)));
 }
 
+function softskillsScript() {
+  let soft_skills_list = document.getElementById("soft-skills");
+  fetch('data/soft_skills.json')
+    .then((response) => response.json())
+    .then((json) => json.forEach((element) => {
+      const badge = soft_skills_list.appendChild(document.createElement("li"));
+      badge.textContent = element
+    })
+  )
+}
+
+function techSkillsScript() {
+  let knowledge_list = document.getElementById("tech-skills");
+  fetch('data/tech_skills.json')
+    .then((response) => response.json())
+    .then((json) => Object.keys(json).forEach((key) => {
+      console.log(key + "> " + json[key]);
+      const badge = knowledge_list.appendChild(document.createElement("li"));
+      badge.textContent = `${key} -> ${json[key]}`;
+      // const badge = soft_skills_list.appendChild(document.createElement("li"));
+      // badge.textContent = element
+    })
+  )
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   loadPartial('navbar', 'partials/header.html');
   loadPartial('sidebar', 'partials/sidebar.html');
 
   projectsScript();
+  softskillsScript();
+  techSkillsScript();
 
   // Get the button:
   let mybutton = document.getElementById("scroll-to-top-button");
